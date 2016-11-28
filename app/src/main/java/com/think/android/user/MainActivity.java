ed7bfe3dfc1b419034c3b4bc617c61819ab5da9a
Flamehaze <longxiao1115@qq.com>
2016-11-28 22:20:22 +0800

    change little

--- app/src/main/java/com/think/android/user/MainActivity.java
+++ app/src/main/java/com/think/android/user/MainActivity.java
@@ -2,8 +2,6 @@ package com.think.android.user;
 
         import java.io.BufferedReader;
         import java.io.File;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
         import java.io.PrintWriter;
         import java.net.ServerSocket;
         import java.net.Socket;
